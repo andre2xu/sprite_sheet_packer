2286b7dc73414109cbd621d10764e85cdd1234b4
andre2xu <>
2024-11-20 19:37:57 +0000

    Configured the main window and added the menubar component to it.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
 import sys
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
+
+# GUI components
+import components.shared as shared
+from components.Menubar import Menubar
 
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        central_widget = shared.VerticalBoxLayout()
+        central_widget.lyt.setContentsMargins(0,0,0,0)
+
+        central_widget.addWidgets([
+            Menubar(),
+            QtWidgets.QLabel('Workspace')
+        ])
+
+        self.setCentralWidget(central_widget)
+        self.setMinimumSize(QtCore.QSize(800, 500))
+        self.setWindowTitle('Sprite Sheet Packer')
 
 
 
