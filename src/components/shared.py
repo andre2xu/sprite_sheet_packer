56f2924006465310d6f2eb2d9f361da9ed7147cc
andre2xu <>
2024-11-20 19:36:05 +0000

    Added a method in the base layout shared widget class to fix stylesheets for layouts.

--- src/components/shared.py
+++ src/components/shared.py
@@ -1,4 +1,4 @@
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui
 
 
 
@@ -12,6 +12,21 @@ class Layout(QtWidgets.QWidget):
     def addWidgets(self, widgets: list):
         for i in range(len(widgets)):
             self.lyt.addWidget(widgets[i])
+
+    def paintEvent(self, _):
+        # NOTE: this method is needed for stylesheets to work when inheriting QWidget (source: https://stackoverflow.com/questions/18344135/why-do-stylesheets-not-work-when-subclassing-qwidget-and-using-q-object)
+
+        style_option = QtWidgets.QStyleOption()
+        style_option.initFrom(self)
+
+        painter = QtGui.QPainter(self)
+
+        self.style().drawPrimitive(
+            QtWidgets.QStyle.PrimitiveElement.PE_Widget,
+            style_option,
+            painter,
+            self
+        )
 
 class HorizontalBoxLayout(Layout):
     def __init__(self):
