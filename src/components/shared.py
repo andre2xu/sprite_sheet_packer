1a223c5fb24ef429e3d87f6ca4eb34e86fda59d8
andre2xu <>
2024-11-22 18:53:37 +0000

    Created scrollable widgets.

--- src/components/shared.py
+++ src/components/shared.py
@@ -2,6 +2,7 @@ from PySide6 import QtWidgets, QtGui
 
 
 
+# LAYOUTS
 class Layout(QtWidgets.QWidget):
     def __init__(self, layout: QtWidgets.QLayout):
         super(Layout, self).__init__()
@@ -35,3 +36,25 @@ class HorizontalBoxLayout(Layout):
 class VerticalBoxLayout(Layout):
     def __init__(self):
         super(VerticalBoxLayout, self).__init__(QtWidgets.QVBoxLayout())
+
+
+
+# SCROLLABLE WIDGETS
+class ScrollableWidget(QtWidgets.QScrollArea):
+    def __init__(self, layoutWidget: Layout):
+        super(ScrollableWidget, self).__init__()
+
+        self.lytw = layoutWidget
+        self.setWidget(layoutWidget)
+
+    def addWidgets(self, widgets: list):
+        for i in range(len(widgets)):
+            self.lytw.lyt.addWidget(widgets[i])
+
+class HorizontallyScrollableWidget(ScrollableWidget):
+    def __init__(self):
+        super(HorizontallyScrollableWidget, self).__init__(HorizontalBoxLayout())
+
+class VerticallyScrollableWidget(ScrollableWidget):
+    def __init__(self):
+        super(VerticallyScrollableWidget, self).__init__(VerticalBoxLayout())
