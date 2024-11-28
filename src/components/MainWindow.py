93762198fa9ee13c133d256b1776ee3e5bf40c6d
andre2xu <>
2024-11-28 18:35:41 +0000

    Added actions to the file menu.

--- src/components/MainWindow.py
+++ src/components/MainWindow.py
@@ -10,7 +10,22 @@ class Menubar(QtWidgets.QMenuBar):
     def __init__(self):
         super(Menubar, self).__init__()
 
-        self.addMenu('File')
+        # FILE MENU
+        file_menu = self.addMenu('&File')
+        file_menu.setMinimumWidth(200)
+
+        file_menu_new_sheet = file_menu.addAction('&New sheet...')
+        file_menu_new_sheet.setShortcut('Ctrl+N')
+        file_menu_open_sheet = file_menu.addAction('&Open sheet...')
+        file_menu_open_sheet.setShortcut('Ctrl+O')
+        file_menu_settings = file_menu.addAction('&Settings')
+        file_menu_settings.setShortcut('Ctrl+K')
+        file_menu.addSeparator()
+        file_menu_exit = file_menu.addAction('&Exit')
+
+        self.addMenu(file_menu)
+
+        # HELP MENU
         self.addMenu('Help')
 
         self.setStyleSheet(
@@ -28,6 +43,26 @@ class Menubar(QtWidgets.QMenuBar):
             QMenuBar::item:selected {
                 background-color: #404244;
                 border-radius: 5px;
+            }
+
+            QMenu {
+                background-color: #53585a;
+                padding: 5px;
+            }
+
+            QMenu::separator {
+                height: 1px;
+                background-color: #3f4344;
+                margin: 4px 0px;
+            }
+
+            QMenu::item {
+                color: #fff;
+                padding: 10px 15px;
+            }
+
+            QMenu::item:selected {
+                background-color: #484c4e;
             }
             """
         )
