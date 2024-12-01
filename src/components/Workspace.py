62184372ceceb674e5c5de71fd24dfb26d015836
andre2xu <>
2024-12-01 16:50:00 +0000

    Created a widget to act as the scroll area's body and made it larger than the scroll area so that the scrollbars become visible. Added a placeholder image inside it to represent the packed sprite sheet.

--- src/components/Workspace.py
+++ src/components/Workspace.py
@@ -14,10 +14,30 @@ class SpriteSheetPreview(components.shared.VerticalBoxLayout):
 
         # SCROLL AREA
         scroll_area = QtWidgets.QScrollArea()
+        self.lyt.addWidget(scroll_area)
+
         scroll_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
         scroll_area.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        scroll_area.setWidgetResizable(True)
-        self.lyt.addWidget(scroll_area)
+        scroll_area.setWidgetResizable(False) # make the scroll area resize according to its widget (the scroll area body), not the other way around
+
+        scroll_area_body = QtWidgets.QWidget()
+        scroll_area_body.setStyleSheet('background-color: red;')
+        scroll_area_body.resize(scroll_area.width() * 2, scroll_area.height() * 2) # make the scroll area widget larger than the scroll area so that the scrollbar's appear
+        scroll_area.setWidget(scroll_area_body)
+
+        sprite_sheet = QtWidgets.QLabel(scroll_area_body)
+        sprite_sheet.setPixmap(QtGui.QPixmap(f'{pathlib.Path(__file__).parent.resolve()}/../../local/packed.png'))
+        sprite_sheet.setScaledContents(True)
+
+        # move sprite sheet to the center of the scroll area body & scroll to it
+        sab_center_distanceX = scroll_area_body.width() // 2
+        sab_center_distanceY = scroll_area_body.height() // 2
+        sprite_sheet.move(sab_center_distanceX - (sprite_sheet.width() * 2), sab_center_distanceY - (sprite_sheet.height() * 2))
+
+        sa_hscrollbar = scroll_area.horizontalScrollBar()
+        sa_vscrollbar = scroll_area.verticalScrollBar()
+        sa_hscrollbar.setValue(sa_hscrollbar.maximum() // 2)
+        sa_vscrollbar.setValue(sa_vscrollbar.maximum() // 2)
 
         # PREVIEW BUTTONS
         ssp_buttons = QtWidgets.QWidget(self) # overlay button container on top of scroll area (this works because the scroll area is a child widget of the SSP's layout, while the button container is a child widget of the SSP itself like the layout)
