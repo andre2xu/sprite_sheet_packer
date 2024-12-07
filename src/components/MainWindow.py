ea861be6ad8d744468eba101b34a28eea3d84bf6
andre2xu <>
2024-12-07 13:28:18 +0000

    Removed the menubar's ampersand keyboard shortcuts.

--- src/components/MainWindow.py
+++ src/components/MainWindow.py
@@ -11,35 +11,35 @@ class Menubar(QtWidgets.QMenuBar):
         super(Menubar, self).__init__(parent)
 
         # FILE MENU
-        file_menu = self.addMenu('&File')
+        file_menu = self.addMenu('File')
         file_menu.setMinimumWidth(250)
 
-        file_menu_new_sheet = file_menu.addAction('&New sheet...')
+        file_menu_new_sheet = file_menu.addAction('New sheet...')
         file_menu_new_sheet.setShortcut('Ctrl+N')
-        file_menu_open_sheet = file_menu.addAction('&Open sheet...')
+        file_menu_open_sheet = file_menu.addAction('Open sheet...')
         file_menu_open_sheet.setShortcut('Ctrl+O')
         file_menu.addSeparator()
-        file_menu_save = file_menu.addAction('&Save')
+        file_menu_save = file_menu.addAction('Save')
         file_menu_save.setShortcut('Ctrl+S')
-        file_menu_save_as = file_menu.addAction('&Save to...')
+        file_menu_save_as = file_menu.addAction('Save to...')
         file_menu_save_as.setShortcut('Ctrl+Shift+S')
         file_menu.addSeparator()
-        file_menu_settings = file_menu.addAction('&Settings')
+        file_menu_settings = file_menu.addAction('Settings')
         file_menu_settings.setShortcut('Ctrl+K')
         file_menu.addSeparator()
-        file_menu_exit = file_menu.addAction('&Exit')
+        file_menu_exit = file_menu.addAction('Exit')
         file_menu_exit.triggered.connect(lambda _: self.parent().app.quit())
 
         self.addMenu(file_menu)
 
         # HELP MENU
-        help_menu = self.addMenu('&Help')
+        help_menu = self.addMenu('Help')
         help_menu.setMinimumWidth(180)
 
-        help_menu_tutorial = help_menu.addAction('&Tutorial')
+        help_menu_tutorial = help_menu.addAction('Tutorial')
         help_menu.addSeparator()
-        help_menu_my_channel = help_menu.addAction('&My YouTube channel')
-        help_menu_source_code = help_menu.addAction('&Project repository')
+        help_menu_my_channel = help_menu.addAction('My YouTube channel')
+        help_menu_source_code = help_menu.addAction('Project repository')
 
         self.setStyleSheet(
             """
