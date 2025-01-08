4bb1eb14db34b0b118e77ad874760bf7daa998ab
andre2xu <>
2025-01-08 13:06:33 +0000

    Created a sprite sheet class that requires a file path and added logic to validate the file in its constructor. Added a close method too for closing its pillow image instance.

--- src/components/shared.py
+++ src/components/shared.py
@@ -1,3 +1,5 @@
+import os, mimetypes, PIL
+import PIL.Image
 from PySide6 import QtWidgets, QtGui
 
 
@@ -36,3 +38,26 @@ class HorizontalBoxLayout(Layout):
 class VerticalBoxLayout(Layout):
     def __init__(self, parent=None):
         super(VerticalBoxLayout, self).__init__(QtWidgets.QVBoxLayout(), parent)
+
+
+
+### MISCELLANEOUS ###
+class SpriteSheet():
+    def __init__(self, spriteSheetPath: str):
+        self.sprite_sheet = None
+
+        if os.path.exists(spriteSheetPath):
+            file_name = os.path.basename(spriteSheetPath)
+            extension = os.path.splitext(file_name)[1]
+            mime_type = mimetypes.guess_type(file_name)[0]
+
+            if (extension == '.png' or extension == '.jpg' or extension == '.jpeg') and (mime_type == 'image/png' or mime_type == 'image/jpg' or mime_type == 'image/jpeg'):
+                self.sprite_sheet = PIL.Image.open(spriteSheetPath)
+            else:
+                raise Exception("Invalid sprite sheet. Must be a png or jpeg image.")
+        else:
+            raise FileNotFoundError("The sprite sheet could not be found. Please make sure you're passing a valid path to an existing file.")
+
+    def close(self):
+        if self.sprite_sheet != None:
+            self.sprite_sheet.close()
